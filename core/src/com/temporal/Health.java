1877cd5c39bc3f032faaf25bfaaafddd31cbf59e
James Zhu <jimmylzhu@gmail.com>
2014-05-24 21:08:25 -0500

    Add health component

---
+++ core/src/com/temporal/Health.java
@@ -0,0 +1,13 @@
+import ashley.core.Component;
+
+public class Health extends Component
+{
+    public int current;
+    public int max;
+
+    public Health(int currHealth, int maxHealth)
+    {
+        current = currHealth;
+        max = maxHealth;
+    }
+}
