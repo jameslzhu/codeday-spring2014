2bf3a5e4d167190f00c5994cd730c5a1b1f8bc09
James Zhu <jimmylzhu@gmail.com>
2014-05-24 23:57:40 -0500

    More bugfixes

--- core/src/com/temporal/ControlSystem.java
+++ core/src/com/temporal/ControlSystem.java
@@ -1,7 +1,6 @@
 package com.temporal;
 
 import ashley.core.Entity;
-import ashley.core.Engine;
 import ashley.core.Family;
 import ashley.core.EntitySystem;
 import ashley.core.EntityListener;
@@ -10,16 +9,22 @@ import ashley.utils.IntMap;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.Gdx;
 
-public class ControlSystem
+public class ControlSystem extends EntitySystem
 {
+    private Engine engine;
+    public ControlSystem(int priority)
+    {
+        super(priority);
+    }
+
     public void processEntity (Entity entity, float deltaTime)
     {
         Position position = entity.getComponent(Position.class);
         Velocity velocity = entity.getComponent(Velocity.class);
-        
+
         velocity.x = 0;
         velocity.y = 0;
-        
+
         if (Gdx.input.isKeyPressed(Keys.LEFT) || Gdx.input.isKeyPressed(Keys.A))
         {
             velocity.x = -1;
@@ -39,24 +44,22 @@ public class ControlSystem
         {
             velocity.y = -1;
         }
-        
+
         if (Gdx.input.isTouched())
         {
-            int mousex = Gdx.input.getX();
-            int mousey = Gdx.input.getY();
-            double deltax = mousex - position.x;
-            double deltay = mousey - position.y;
+            double deltax = Gdx.input.getX() - position.x;
+            double deltay = Gdx.input.getY() - position.y;
             double length = Math.sqrt(deltax * deltax + deltay * deltay);
             deltax /= length;
             deltay /= length;
-            
+
             deltax *= 3;
             deltay *= 3;
-            Entity bullet = new Entity();
+
             Position bulletPos = new Position(position.x, position.y);
-            bullet.add(bulletPos);
             Velocity bulletVel = new Velocity(deltax, deltay);
-            bullet.add(bulletVel);
+            PlayerBullet damage = new PlayerBullet(5);
+            engine.addPlayerBullet(bulletPos, bulletVel, damage);
         }
     }
 }
--- core/src/com/temporal/PlayerBullet.java
+++ core/src/com/temporal/PlayerBullet.java
@@ -5,4 +5,8 @@ import ashley.core.Component;
 public class PlayerBullet extends Component
 {
     public int damage;
+    public PlayerBullet(int dmg)
+    {
+        damage = dmg;
+    }
 }
