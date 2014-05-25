d78b80205630d118b5b8a6ac7538e0ee49e5a10d
MichaelV352 <mikev@wi.rr.com>
2014-05-24 20:43:36 -0500

    Velocity and MovementSystem made

--- core/src/com/temporal/MovementSystem.java
+++ core/src/com/temporal/MovementSystem.java
@@ -6,3 +6,17 @@ import ashley.core.Family;
 import ashley.core.EntitySystem;
 import ashley.core.EntityListener;
 import ashley.utils.IntMap;
+
+public class MovementSystem extends IteratingSystem {
+  public MovementSystem () {
+    super(Family.getFamilyFor(Position.class, Velocity.class));
+  }
+
+  public void processEntity (Entity entity, float deltaTime) {
+    Position position = entity.getComponent(Position.class);
+    Velocity velocity = entity.getComponent(Velocity.class);
+
+    position.x += velocity.x * deltaTime;
+    position.y += velocity.y * deltaTime;
+  }
+}
---
+++ core/src/com/temporal/Velocity.java
@@ -0,0 +1,12 @@
+public class Velocity extends Component
+{
+	public double x;
+	public double y;
+	public Velocity(double newX, double newY)
+	{
+		x = newX;
+		y = newY;
+	}
+}
+
+
