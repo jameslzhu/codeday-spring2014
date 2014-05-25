6fa5a3e0772ac102c7db6b1c8e8022402f27bb9b
James Zhu <jimmylzhu@gmail.com>
2014-05-24 23:40:30 -0500

    Implement factory methods

--- core/src/com/temporal/Engine.java
+++ core/src/com/temporal/Engine.java
@@ -9,18 +9,40 @@ import com.badlogic.gdx.graphics.Texture;
 public class Engine extends PooledEngine
 {
     private Texture playerTex;
+    private Texture playerBulletTex;
+    private Texture enemyTex;
+    private Texture enemyBulletTex;
+
     public Engine(int windowWidth, int windowHeight)
     {
         Entity player = new Entity();
         playerTex = new Texture(Gdx.files.internal("player.png"));
-        player.add(new Graphics(playerTex));
-        player.add(new Position(windowWidth / 2, windowHeight / 2));
-
+        Position playerPos = new Position(windowWidth / 2, windowHeight / 2);
+        Velocity velocity = new Velocity(0.0, 0.0);
+        Health health = new Health(10);
         addEntity(player);
 
         GraphicsSystem graphics = new GraphicsSystem(1, windowWidth, windowHeight, 1);
 
         addSystem(graphics);
+    }
+
+    public void addPlayer(Position pos, Velocity vel, Health health)
+    {
+        Entity player = new Entity();
+        player.add(pos);
+        player.add(vel);
+        player.add(health);
+        double size = 20.0;
+        Polygon shape = new Polygon(
+            0.0, 0.0,
+            size, 0.0,
+            size, size,
+            0.0, size
+        );
+        player.add(new CollisionBox(shape));
+        player.add(playerTex);
+        addEntity(player);
     }
 
     public void addEnemy(Position pos, Velocity vel, Enemy damage, Health health, CollisionBox box)
@@ -31,6 +53,7 @@ public class Engine extends PooledEngine
         enemy.add(damage);
         enemy.add(health);
         enemy.add(box);
+        enemy.add(enemyTex);
         addEntity(enemy);
     }
 
@@ -41,6 +64,7 @@ public class Engine extends PooledEngine
         bullet.add(vel);
         bullet.add(damage);
         bullet.add(box);
+        bullet.add(playerBulletTex);
         addEntity(bullet);
     }
 
@@ -51,12 +75,29 @@ public class Engine extends PooledEngine
         bullet.add(vel);
         bullet.add(damage);
         bullet.add(box);
+        bullet.add(enemyBulletTex);
         addEntity(bullet);
     }
 
     public void dispose()
     {
         playerTex.dispose();
+
+        if (playerBulletTex != null)
+        {
+            playerBulletTex.dispose();
+        }
+
+        if (enemyTex != null)
+        {
+            enemyTex.dispose();
+        }
+
+        if (enemyBulletTex != null)
+        {
+            enemyBulletTex.dispose();
+        }
+
         getSystem(GraphicsSystem.class).dispose();
     }
 }
