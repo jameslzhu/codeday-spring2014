cb1227355811b2beac0ad118859c7179cbe70132
Brian Truong <briantruong777@gmail.com>
2014-05-25 06:10:27 -0500

    more time fun

--- core/src/com/temporal/ControlSystem.java
+++ core/src/com/temporal/ControlSystem.java
@@ -16,6 +16,7 @@ public class ControlSystem extends EntitySystem
     private Entity player;
     private Engine engine;
     private Signal<Boolean> signal;
+    private float fireCooldown;
 
     public ControlSystem(int priority, Entity player, Engine engine, Signal<Boolean> signal)
     {
@@ -23,6 +24,7 @@ public class ControlSystem extends EntitySystem
         this.player = player;
         this.engine = engine;
         this.signal = signal;
+        fireCooldown = 0.0f;
     }
 
     public void update (float deltaTime)
@@ -33,49 +35,58 @@ public class ControlSystem extends EntitySystem
         velocity.x = 0;
         velocity.y = 0;
 
-        boolean moving = false;
+        boolean fastmo = false;
 
         if (Gdx.input.isKeyPressed(Keys.LEFT) || Gdx.input.isKeyPressed(Keys.A))
         {
-            velocity.x = -100;
-            moving = true;
+            velocity.x = -25;
+            fastmo = true;
         }
 
         if (Gdx.input.isKeyPressed(Keys.RIGHT) || Gdx.input.isKeyPressed(Keys.D))
         {
-            velocity.x = 100;
-            moving = true;
+            velocity.x = 25;
+            fastmo = true;
         }
 
         if (Gdx.input.isKeyPressed(Keys.UP) || Gdx.input.isKeyPressed(Keys.W))
         {
-            velocity.y = 100;
-            moving = true;
+            velocity.y = 25;
+            fastmo = true;
         }
 
         if (Gdx.input.isKeyPressed(Keys.DOWN) || Gdx.input.isKeyPressed(Keys.S))
         {
-            velocity.y = -100;
-            moving = true;
+            velocity.y = -25;
+            fastmo = true;
         }
 
-        signal.dispatch(moving);
-
-        if (Gdx.input.isTouched())
+        if (fireCooldown <= 0.0f)
         {
-            double deltax = Gdx.input.getX() - position.x;
-            double deltay = (Gdx.graphics.getHeight() - Gdx.input.getY()) - position.y;
-            double length = Math.sqrt(deltax * deltax + deltay * deltay);
-            deltax /= length;
-            deltay /= length;
+          if (Gdx.input.isTouched())
+          {
+              double deltax = Gdx.input.getX() - position.x;
+              double deltay = (Gdx.graphics.getHeight() - Gdx.input.getY()) - position.y;
+              double length = Math.sqrt(deltax * deltax + deltay * deltay);
+              deltax /= length;
+              deltay /= length;
 
-            deltax *= 100;
-            deltay *= 100;
+              deltax *= 100;
+              deltay *= 100;
 
-            Position bulletPos = new Position(position.x + 20.0, position.y + 20.0);
-            Velocity bulletVel = new Velocity(deltax, deltay);
-            PlayerBullet damage = new PlayerBullet(5);
-            engine.addPlayerBullet(bulletPos, bulletVel, damage);
+              Position bulletPos = new Position(position.x + 20.0, position.y + 20.0);
+              Velocity bulletVel = new Velocity(deltax, deltay);
+              PlayerBullet damage = new PlayerBullet(5);
+              engine.addPlayerBullet(bulletPos, bulletVel, damage);
+              fireCooldown = 0.5f;
+          }
         }
+        else
+        {
+          fireCooldown -= deltaTime;
+          fastmo = true;
+        }
+
+        signal.dispatch(fastmo);
     }
 }
